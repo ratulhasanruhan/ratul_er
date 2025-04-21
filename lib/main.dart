2365f8400f4dd3b504cea7f8954251cc189a075f
Ratul Hasan Ruhan <ratulhasan1644@gmail.com>
2025-04-21 07:01:30 +0600

    responsive breakpoints

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ratul_er/utils/colors.dart';
 import 'package:ratul_er/utils/constants.dart';
 import 'package:ratul_er/utils/routes.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'firebase_options.dart';
 
 void main() async{
@@ -33,7 +35,17 @@ class MyApp extends StatelessWidget {
           surface: accentColor,
           error: secondaryColor,
         ),
+        fontFamily: GoogleFonts.poppins().fontFamily,
         useMaterial3: true,
+      ),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
       ),
       debugShowCheckedModeBanner: false,
       defaultTransition: Transition.cupertino,
